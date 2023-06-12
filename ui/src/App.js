55eac5bdc80172a799414ab1b9b14ddebe45bb2a
denhampreen <denhampreen@gmail.com>
2023-06-12 21:21:35 +0200

    layout: landing view

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React from "react";
+import Search from "./components/svgs/Search";
+import NFTCollectionCard from "./components/NFTCollectionCard";
 
 function App() {
   return (
     <div className="flex flex-col h-full items-center justify-center bg-gray-200 text-gray-700">
-      <div className="flex items-center">
-        <h1 className="text-6xl font-thin tracking-wider">Create React App + Tailwind CSS</h1>
+      <div className="flex flex-col items-center">
+        <h1 className="text-6xl font-thin tracking-wider">
+          Pooltogether birbwatcher
+        </h1>
       </div>
       <p className="my-6 tracking-wide">
-        Edit <code>src/App.js</code> and save to reload.
+        <div className="flex flex-row w-full">
+          <input
+            placeholder="Search a collectooors address here 🐦"
+            className="w-[80%] min-w-[500px] p-4 border-1 rounded-lg"
+          />
+          <div className="w-[60px] ml-4 p-2 border-1 rounded-lg bg-green-300">
+            <Search />
+          </div>
+        </div>
       </p>
-      <div className="mt-6 flex justify-center">
-        <a
-          className="uppercase hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="ml-10 uppercase hover:underline"
-          href="https://tailwindcss.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tailwind
-        </a>
+      <div className="mt-6 flex flex-row justify-center">
+        <NFTCollectionCard />
+        <NFTCollectionCard />
+        <NFTCollectionCard />
+        <NFTCollectionCard />
+        <NFTCollectionCard />
       </div>
     </div>
   );
---
+++ ui/src/components/NFTCollectionCard.js
@@ -0,0 +1,9 @@
+const NFTCollectionCard = () => {
+  return (
+    <div className="w-[100px] h-[100px] bg-gray-400 m-4 border-1 rounded-lg">
+      🐦
+    </div>
+  );
+};
+
+export default NFTCollectionCard;
